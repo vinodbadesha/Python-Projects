7f6759210dcc19b8e397fc1b583a1af7593f16c1
Vinod Badesha <badeshavinod@gmail.com>
2024-10-23 21:02:30 +0530

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,12 @@
+import time
+
+timer = int(input("Enter the time in seconds: "))
+
+for i in (range(timer, 0, -1)):
+    seconds = i % 60
+    minutes = int(i / 60) % 60
+    hours = int(i / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time's UP")
